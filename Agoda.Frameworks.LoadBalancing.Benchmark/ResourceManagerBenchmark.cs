2f73a0c68822063291939a2e094b1fa9ac555431
Kanes Kemiganithi <Kanes.kemiganithi@agoda.com>
2019-01-30 12:18:15 +0700

    add benchmark

---
+++ Agoda.Frameworks.LoadBalancing.Benchmark/ResourceManagerBenchmark.cs
@@ -0,0 +1,38 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+
+namespace Agoda.Frameworks.LoadBalancing.Benchmark
+{
+    
+    [CsvMeasurementsExporter, CsvExporter, RPlotExporter]
+    public class ResourceManagerBenchmark
+    {
+
+        [Params(100, 500, 1000)]
+        public int weight;
+
+        [Benchmark]
+        public void UpdateWeight_Benchmark()
+        {
+            var dict = new Dictionary<string, WeightItem>
+            {
+                {"url1", new WeightItem(weight, 1000)}
+            }.ToImmutableDictionary();
+
+            var resourceManager = new ResourceManager<string>(dict, new AgodaWeightManipulationStrategy());
+            resourceManager.UpdateWeight("url1", true);
+        }
+
+        [Benchmark]
+        public void UpdateResource_Benchmark()
+        {
+            var urls = new List<string> { "url1", "url2", "url3", "url4" };
+            var dict = urls.ToImmutableDictionary(x => x, x => WeightItem.CreateDefaultItem());
+            var resourceManager = new ResourceManager<string>(dict, new AgodaWeightManipulationStrategy());
+            resourceManager.UpdateResources(dict);
+        }
+    }
+}
