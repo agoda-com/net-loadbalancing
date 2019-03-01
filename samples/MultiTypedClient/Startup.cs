fa8ea71a19964c69d035afe3b620a4a6f16af48c
Ting-Yu Lin <ting-yu.lin@agoda.com>
2019-03-01 17:46:35 +0700

    Add net462 target

--- samples/MultiTypedClient/Startup.cs
+++ samples/MultiTypedClient/Startup.cs
@@ -28,7 +28,7 @@ namespace MultiTypedClient
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddHttpClient<IStackExchangeClient, StackExchangeClient>("stackexchange", c =>
             {
