b435a842bb10a3bb88b26f097c049a3aca47b177
Joel Dickson <Joel.Dickson@agoda.com>
2022-01-28 08:40:28 +0700

    Add benchmarking

---
+++ Agoda.Frameworks.LoadBalancing.Benchmark/Config.cs
@@ -0,0 +1,18 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Exporters.Csv;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Agoda.Frameworks.LoadBalancing.Benchmark
+{
+    public class Config : ManualConfig
+    {
+        public Config()
+        {
+            Add(CsvMeasurementsExporter.Default);
+            Add(RPlotExporter.Default);
+        }
+    }
+}
