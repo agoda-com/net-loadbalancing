b954f8c05cc10cd3ba3440e6a3bcffdfc1b74d84
Abdullah Al Tariq <abdullah.tariq@agoda.com>
2022-12-09 21:39:46 +0700

    Throw request timeout exception when provided timeout expires

---
+++ Agoda.Frameworks.Http.Tests/MockSetup/WaitedMockHttpMessageHandler.cs
@@ -0,0 +1,29 @@
+﻿using RichardSzalay.MockHttp;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Agoda.Frameworks.Http.Tests.MockSetup
+{
+    public class WaitedMockHttpMessageHandler : MockHttpMessageHandler
+    {
+        private readonly TimeSpan waitTime;
+
+        public WaitedMockHttpMessageHandler(TimeSpan waitTime, BackendDefinitionBehavior backendDefinitionBehavior = BackendDefinitionBehavior.NoExpectations)
+            : base(backendDefinitionBehavior)
+        {
+            this.waitTime = waitTime;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var timeoutTask = Task.Delay(waitTime, cancellationToken);
+            var respTask = base.SendAsync(request, cancellationToken);
+
+            await Task.WhenAll(timeoutTask, respTask);
+
+            return await respTask;
+        }
+    }
+}
